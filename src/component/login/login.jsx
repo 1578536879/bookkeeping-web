c021be3271716611dbd712f6f8de008caf14cc5a
xinyang1328 <xinyang1328@gmail.com>
2020-07-09 10:54:13 +0800

    注册登录接口

--- src/component/login/login.jsx
+++ src/component/login/login.jsx
@@ -3,6 +3,10 @@ import {Input, Button, Form} from 'antd'
 import {LockOutlined, MailOutlined} from '@ant-design/icons'
 import {withRouter} from 'react-router-dom'
 import './login.css'
+import Axios from 'axios'
+import qs from 'qs'
+import enviornment from '../../common/enviornment'
+import md5 from 'js-md5'
 
 class Login extends Component{
     constructor(props){
@@ -11,45 +15,58 @@ class Login extends Component{
             email: '',
             password: ''
         }
-        this.emailChange = this.emailChange.bind(this)
-        this.passwordChange = this.passwordChange.bind(this)
         this.login = this.login.bind(this)
     }
 
-    emailChange = (e) =>{
-        this.setState({
-            email: e.target.value
-        })
-    }
-
-    passwordChange = (e) =>{
-        this.setState({
-            password: e.target.value
-        })
-    }
 
     login(){
-        console.log(this.props)
-        this.props.history.push({
-            pathname: '/home',
-            query: {
-                data: '123124'
-            }
+        let that = this
+        let password = that.refs.password.props.value || that.refs.password.state.value
+        let pos = Math.floor(Math.random()*password.length)
+        password = password.substr(0, pos) + md5('BookKeeping') + password.substr(pos, password.length - pos)
+        password = btoa(password)
+        this.setState({
+            email: that.refs.email.props.value || that.refs.email.state.value,
+            password: password
         })
+        Axios({
+            method: 'post',
+            url: `http://${enviornment.hostname}:${enviornment.port}/login`,
+            data: qs.stringify({
+                email: that.state.email,
+                password: that.state.password,
+                pos: pos
+            }),
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+        }).then(res => {
+            console.log(res)
+            localStorage.setItem("token", res.data.data.token)
+            if(res.data.code === 200){
+                this.props.history.push({
+                    pathname: '/test',
+                })
+            }
+            
+        }).catch(err=>{
+            console.log(err)
+        })
+       
     }
 
     render(){
         return (
-            <Form className='login' style={{margin: 'auto'}} onClick={this.login}>
+            <Form className='login' style={{margin: 'auto'}} onFinish={this.login}>
                 <Form.Item name='email' rules={[{
                     required: true,
                     pattern: /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
                     message: '邮箱格式错误'
                 }]}>
-                    <Input prefix={<MailOutlined />} placeholder='请输入邮箱' value={this.state.email}></Input>
+                    <Input prefix={<MailOutlined />} placeholder='请输入邮箱' value={this.state.email}  ref="email"></Input>
                 </Form.Item>
                 <Form.Item name="password">
-                    <Input.Password prefix={<LockOutlined/>}  type="password" placeholder="请输入密码" value={this.state.password} />
+                    <Input.Password prefix={<LockOutlined/>}  type="password" placeholder="请输入密码" value={this.state.password}  ref="password"/>
                 </Form.Item>
                 <Form.Item  className='forget'>
                     <span className="login-form-forgot">忘记密码</span>
--- src/component/register/register.jsx
+++ src/component/register/register.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
-import {Input, Button, Form} from 'antd'
+import {Input, Button, Form, message} from 'antd'
 import {LockOutlined, MailOutlined, UserOutlined } from '@ant-design/icons'
 import './register.css'
+import Axios from 'axios'
+import qs from 'qs'
+import enviorment from '../../common/enviornment'
+import md5 from 'js-md5'
 
 export default class Register extends Component{
     constructor(props){
         super(props)
+        this.register = this.register.bind(this)
+        this.getCode = this.getCode.bind(this)
         this.state = {
             email: '',
             password: '',
             code: '',
             username: '',
-            codeBnt: 'primary',
+            codeDisabled: false,
+            codeMsg: '点击获取验证码',
             codeStyle: {
                 width: '55%',
                 marginRight: '4%',
@@ -20,50 +27,113 @@ export default class Register extends Component{
         }
     }
 
-    emailChange = (e) => {
+    getCode = (e) =>{
+        console.log(enviorment)
+        let email = this.refs.email.props.value || this.refs.email.state.value
+        if(!email){
+            message.error('请输入您的邮箱地址!')
+            return 
+        }
         this.setState({
-            email: e.target.value
+            email: email
         })
-    }
-
-    passwordChange = (e) => {
+        let that = this
+        let second = 60
+        let interval = setInterval(function(){
+            second--
+            that.setState({
+                codeMsg: second + "s后重新获取"
+            })
+        }, 1000)
+        setTimeout(function(){
+            clearInterval(interval)
+            that.setState({
+                codeDisabled: false,
+                codeMsg: '点击获取验证码'
+            })
+        }, 1000 * 60)
         this.setState({
-            password: e.target.value
+            codeDisabled: true,
+            codeMsg: second + "s后重新获取"
         })
-    }
-
-    codeChange = (e) =>{
-        this.setState({
-            code: e.target.value
+        Axios({
+            method: 'post',
+            url: `http://${enviorment.hostname}:${enviorment.port}/getRegisterCode`,
+            data: qs.stringify({
+                email: email
+            }),
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+        }).then(res=>{
+            console.log(res)
+            if(res.data.code === 200){
+                message.success('验证码发送成功，请到邮箱查看~')
+            }
         })
     }
 
     register(){
         console.log(this.state)
+        let password = this.refs.password.props.value || this.refs.password.state.value
+        let pos = Math.floor(Math.random()*password.length)
+        password = password.substr(0, pos) + md5('BookKeeping') + password.substr(pos, password.length - pos)
+        password = btoa(password)
+        this.setState({
+            password: password,
+            email: this.refs.email.props.value || this.refs.email.state.value,
+            username: this.refs.username.props.value || this.refs.username.state.value,
+            code: this.refs.code.props.value || this.refs.code.state.value
+        })
+        let that = this
+        Axios({
+            method: 'post',
+            url: `http://${enviorment.hostname}:${enviorment.port}/register`,
+            data: qs.stringify({
+                email: that.state.email,
+                password: that.state.password,
+                username: that.state.username,
+                code: that.state.code,
+                pos: pos
+            }),
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+        }).then(res=>{
+            console.log(res)
+        })
     }
 
     render(){
         return (
-            <Form className='register' style={{margin: 'auto'}} onClick={this.register} >
+            <Form className='register' style={{margin: 'auto'}} onFinish={this.register} >
                 <Form.Item name='email' rules={[{
                     required: true,
                     pattern: /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
                     message: '邮箱格式错误'
                 }]} style={{marginBottom: '10px'}}>
-                    <Input prefix={<MailOutlined />} placeholder='请输入邮箱' value={this.state.email}></Input>
+                    <Input prefix={<MailOutlined />} placeholder='请输入邮箱' ref='email'></Input>
                 </Form.Item>
-                <Form.Item style={{marginBottom: '10px'}}>
-                    <Input prefix={<UserOutlined />}  type="password" placeholder="请输入用户名" value={this.state.username} />
+                <Form.Item style={{marginBottom: '10px'}} name='username' rules={[{
+                    required: true,
+                    pattern: /^[A-Za-z0-9_]{3,32}$/,
+                    message: '请输入包含大小写字母、数字和下划线,长度为3~32的用户名'
+                }]}>
+                    <Input prefix={<UserOutlined />} placeholder="请输入用户名" ref='username'/>
                 </Form.Item>
-                <Form.Item name="password" style={{marginBottom: '10px'}}>
-                    <Input.Password prefix={<LockOutlined/>}  type="password" placeholder="请输入密码" value={this.state.password} />
+                <Form.Item name="password" style={{marginBottom: '10px'}} rules={[{
+                    required: true,
+                    pattern: /((?=.*[0-9])(?=.*[A-Za-z]))|((?=.*[0-9])(?=.*[!~@#$%^&*_.]))|((?=.*[A-Za-z])(?=.*[!~@#$%^&*_.]))^[0-9A-Za-z!~@#$%^&*_.]{6,32}$/,
+                    message: '请输入长度为3~32的包含大小写字母、数字和特殊字符两种以上的密码'
+                }]}>
+                    <Input.Password prefix={<LockOutlined/>}  type="password" placeholder="请输入密码" ref='password'/>
                 </Form.Item>
 
-                <Form.Item style={{marginBottom: '10px'}}>
-                    <Input value={this.state.code} placeholder='验证码' style={{width: '53%', marginRight: '5%',}}></Input>
-                    <Button type="primary" className='codeBnt'>获取验证码</Button>
+                <Form.Item style={{marginBottom: '10px'}} name='code'>
+                    <Input placeholder='验证码' style={{width: '53%', marginRight: '5%'}} ref='code'></Input>
+                    <Button type="primary" className='codeBnt' disabled={this.state.codeDisabled} onClick={this.getCode} style={{padding: '0'}}>{this.state.codeMsg}</Button>
                 </Form.Item>
-                <Form.Item>
+                <Form.Item name='register'>
                     <Button type="primary" htmlType="submit" block>注册</Button>
                 </Form.Item>
             </Form>
